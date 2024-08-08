535ea4c5c6bd56fe61b8329525221bd25e6d456c
Vladimir902 <vladimir.malakovski@kinandcarta.com>
2024-08-08 15:41:08 +0200

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
+import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
         String str = "Nestooo";
         char[] ch = str.toCharArray();
 
