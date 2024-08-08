0523147cac77837cda424d5be147913beb4d9d05
Vladimir902 <vladimir.malakovski@kinandcarta.com>
2024-08-08 15:04:33 +0200

    initial commmitse

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = "Nesto";
+        char[] ch = str.toCharArray();
+
+        System.out.println(str.indexOf("t"));
+    }
+}
