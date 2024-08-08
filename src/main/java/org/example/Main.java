a0b645c9c32f83358a9fdabc6cb36065b981c258
Vladimir902 <vladimir.malakovski@kinandcarta.com>
2024-08-08 15:22:08 +0200

    seecond commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        String str = "Nesto";
+        String str = "Nestooo";
         char[] ch = str.toCharArray();
 
         System.out.println(str.indexOf("t"));
