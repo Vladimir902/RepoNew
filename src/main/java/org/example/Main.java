0a208f85b8382bbc9e6be10b1b2d6618f3465281
Vladimir902 <vladimir.malakovski@kinandcarta.com>
2024-08-09 13:15:58 +0200

    Reverted the project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,10 @@
 package org.example;
-import java.util.Scanner;
-
 
 public class Main {
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
         String str = "Nestooo";
         char[] ch = str.toCharArray();
 
         System.out.println(str.indexOf("t"));
-
-        for (char arr : ch) {
-            System.out.println(ch);
-        }
     }
 }
